271a2e45cc4a29bd0adda4dabd9c99c1a334027b
EnriqueB720 <enrique21102001@hotmail.com>
2026-01-29 20:56:38 -0600

    Adding initial config files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './shared/config/config.service';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
+import { config } from 'dotenv'
+
+config({ path: '.env.local' })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get<ConfigService>(ConfigService);
+
+  const PORT = config.get('PORT') as number;
+
+  app.enableCors();
+
+  await app.listen(PORT);
+  
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '150mb' }));
+
+  Logger.log(`App Running in port ${PORT}`, 'App Initialization');
 }
 bootstrap();
